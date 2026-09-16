fe5ebdbf3dd3acd1d8979548161c6f29dc80ead7
dwightsckrute <162919718+dwightsckrute@users.noreply.github.com>
2026-09-16 14:37:12 +0200

    Prepara Visto 2.0 beta 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.dwightsckrute.visto"
         minSdk = 24
         targetSdk = 36
-        versionCode = 26
-        versionName = "2.0"
+        versionCode = 27
+        versionName = "2.0.1-beta.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
