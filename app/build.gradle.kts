52a2a3911849893a98c9ab2293da25f96b961ece
dwightsckrute <162919718+dwightsckrute@users.noreply.github.com>
2026-09-16 14:59:29 +0200

    Corrige el icono de Visto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.dwightsckrute.visto"
         minSdk = 24
         targetSdk = 36
-        versionCode = 27
-        versionName = "2.0.1-beta.13"
+        versionCode = 28
+        versionName = "2.0.1-beta.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
